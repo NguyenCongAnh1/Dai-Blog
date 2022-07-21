d678ea2cfc13b37c3ccb12c0feeed984b850f31b
your name <nguyenanh342000@gmail.com>
2022-07-21 22:45:54 +0700

    change sqlite to postgreSql

--- main.py
+++ main.py
@@ -21,10 +21,15 @@ from forms import CreatePostForm, CreateRegisterForm, LoginForm, CommentForm
 from flask_gravatar import Gravatar
 
 
+import os
+
 
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+# '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 ckeditor = CKEditor(app)
 gravatar = Gravatar(app, size=100, rating='g', default='retro', force_default=False, force_lower=False, use_ssl=False, base_url=None)
 Bootstrap(app)
